4119aabcc73c3bc55ee2b14151dd4e0310b10a49
emilywhitaker15@gmail.com <emilywhitaker15@gmail.com>
2018-05-23 17:48:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   getCart()
   if (cardNumber !== undefined) {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-    cart = []
-    return cart
+    var purchase = total()
+    cart.length = 0
+    return `Your total cost is $${purchase}, which will be charged to the card ${cardNumber}.`
   }
   else {
     return "Sorry, we don't have a credit card on file for you."
