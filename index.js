df5e7441eb6a00e17199fe5b28b8b5bf41ee28bd
emilywhitaker15@gmail.com <emilywhitaker15@gmail.com>
2018-05-23 17:43:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function placeOrder(cardNumber) {
   getCart()
   if (cardNumber !== undefined) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-    cart.splice(0, cart.length)
+    cart = []
   }
   else {
     return "Sorry, we don't have a credit card on file for you."
