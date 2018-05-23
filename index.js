77f49eaccff4cd9967e4d54152827fc788b8e4f7
emilywhitaker15@gmail.com <emilywhitaker15@gmail.com>
2018-05-23 17:14:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
   getCart()
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.prototype.splice(i, 1)
+      cart.splice(i, 1)
     }
     else {
       return "That item is not in your cart."
